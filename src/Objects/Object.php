dbfc9acf2ccddc61fb4634d3378fa3a97435dd38
Justas Lavišius <jl@itcity.lt>
2017-11-24 17:35:12 +0200

    Object: extend Laravel Model class

--- src/Objects/Object.php
+++ src/Objects/Object.php
@@ -6,37 +6,60 @@ use ITCity\Rivile\Exceptions\RivileInvalidAttribute;
 use ITCity\Rivile\Exceptions\RivileInvalidObject;
 use ITCity\Rivile\QueryBuilder;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Concerns\HasAttributes;
+use Illuminate\Database\Eloquent\Model;
+use SimpleXMLElement;
 
-class Object {
-	use HasAttributes {
-		getAttribute as public baseGetAttribute;
-		setAttribute as public baseSetAttribute;
-	}
+class Object extends Model{
 
 	protected static $prefix;
 	protected static $primary_key;
 	protected static $query_method;
 	protected static $defs;
+	protected static $relation_map;
 
-	public function __construct ($data = array()) {
-		if (static::$defs) {
-			foreach (static::$defs as $field => &$rules) {
-				$rules[] = 'nullable';
-			}
-			if ($data) {
-				$data = array_change_key_case($data);
-				$validator = \Validator::make($data, static::$defs);
-				if ($validator->fails()) {
-					$failed = [];
-					foreach ($validator->failed() as $field => $rules) {
-						$failed[] = "<{$field}>: <".print_r(array_get($data, $field), true)."> fails [".join(', ', array_keys($rules))."]";
+	protected $dateFormat = "Y-m-d H:i:s.u";
+	protected $table = '';
+
+	protected static function boot() {
+        parent::bootTraits();
+
+        foreach (static::$defs as $field => &$rules) {
+			$rules[] = 'nullable';
+		}
+
+		static::retrieved(function($object) {
+			$attributes = $object->getAttributes();
+			foreach ((array) $object::$relation_map as $key => $def) {
+				if (isset($attributes[$key])) {
+					if (array_get($def, 'single')) {
+						$relation = $def['class']::instantiate($attributes[$key]);
+					} else {
+						$relation = $def['class']::fromList($attributes[$key]);
 					}
-					throw new RivileInvalidObject(join('; ', $failed));
+					$object->setRelation($def['name'], $relation);
+					unset($object->$key);
 				}
 			}
+		});
+    }
+
+    public function getFillable() {
+    	return array_keys(static::$defs);
+    }
+
+	public function fill (array $data) {
+		if (!$data) return;
+		$data = array_change_key_case($data);
+		$validator = \Validator::make($data, static::$defs);
+		if ($validator->fails()) {
+			$failed = [];
+			foreach ($validator->failed() as $field => $rules) {
+				$failed[] = "<{$field}>: <".print_r(array_get($data, $field), true)."> fails [".join(', ', array_keys($rules))."]";
+			}
+			throw new RivileInvalidObject(join('; ', $failed));
 		}
-		$this->attributes = $data;
+		
+		return parent::fill($data);
 	}
 
 	protected static function prefixedName ($name) {
@@ -89,6 +112,10 @@ class Object {
 		return isset(static::$query_method) ? static::$query_method : "GET_".static::$prefix."_LIST";
 	}
 
+	public static function getEditMethod () {
+		return isset(static::$edit_method) ? static::$edit_method : 'EDIT_'.static::$prefix;
+	}
+
 	public static function getPrimaryKey () {
 		if (isset(static::$primary_key)) {
 			return static::$primary_key;
@@ -98,27 +125,75 @@ class Object {
 		}
 	}
 
-	public static function query () {
-		return new QueryBuilder(static::class);
+	public function newQueryWithoutScopes () {
+		return new QueryBuilder($this);
 	}
 
-	public static function where (...$params) {
-		return (new QueryBuilder(static::class))->where(...$params);
-	}
-
-	public static function find ($value) {
-		return (new QueryBuilder(static::class))->find($value);
+	public static function instantiate ($raw_data) {
+		return (new static)->newFromBuilder(array_change_key_case($raw_data));
 	}
 
 	public static function fromList ($list) {
 		$out = collect();
 		foreach ($list as $item) {
-			$out->push(new static($item));
+			if (!$item) continue;
+			$out->push(static::instantiate($item));
 		}
 		return $out;
 	}
 
+	public function save(array $options = [])
+    {
+        $query = $this->newQueryWithoutScopes();
+
+        // If the "saving" event returns false we'll bail out of the save and return
+        // false, indicating that the save failed. This provides a chance for any
+        // listeners to cancel save operations if validations fail or whatever.
+        if ($this->fireModelEvent('saving') === false) {
+            return false;
+        }
+
+        // If the model already exists in the database we can just update our record
+        // that is already in this database using the current IDs in this "where"
+        // clause to only update this model. Otherwise, we'll just insert them.
+        if ($this->exists) {
+            $saved = $this->isDirty() ?
+                        $query->update() : true;
+        }
+
+        // If the model is brand new, we'll insert it into our database and set the
+        // ID attribute on the model to the value of the newly inserted row's ID
+        // which is typically an auto-increment value managed by the database.
+        else {
+            $saved = $query->insert();
+        }
+
+        // If the model is successfully saved, we need to do a few more things once
+        // that is done. We will call the "saved" method here to run any actions
+        // we need to happen after a model gets successfully saved right here.
+        if ($saved) {
+            $this->finishSave($options);
+        }
+
+        return $saved;
+    }
+
+	public function toXml () {
+		$simple_xml = array_to_xml($this->attributesToArray(), new SimpleXMLElement('<'.static::$prefix.' />'));
+		$dom = dom_import_simplexml($simple_xml);
+		return $dom->ownerDocument->saveXML($dom->ownerDocument->documentElement);
+	}
+
+	public function getRelationNames() {
+		return collect((array) static::$relation_map)->mapWithKeys(function($item) {
+			return [$item['name'] => $item['class']];
+		});
+	}
+
 	public function __get($key) {
+		if (array_get($this->getRelationNames(), $key)) {
+			return $this->getRelation($key);
+		}
         return $this->getAttribute($key);
     }
 
@@ -126,12 +201,19 @@ class Object {
         $this->setAttribute($key, $value);
     }
 
+    public function __call ($method, $parameters) {
+    	if ($class = array_get($this->getRelationNames(), $method)) {
+    		return new $class;
+    	}
+    	return parent::__call($method, $parameters);
+    }
+
     public function getAttribute ($key) {
-	    return $this->baseGetAttribute(static::attrName($key));
+	    return parent::getAttribute(static::attrName($key));
     }
 
     public function setAttribute ($key, $value) {
-    	return $this->baseSetAttribute(static::attrName($key), value);
+    	return parent::setAttribute(static::attrName($key), $value);
     }
 
     public function relationLoaded($key) {
@@ -147,9 +229,9 @@ class Object {
     }
 
     public function getDates() {
-        return $this->usesTimestamps()
-                    ? array_unique(array_merge($this->dates, $defaults))
-                    : $this->dates;
+        return collect(static::$defs)->filter(function($item) {
+        	return array_get($item, 0) == 'date';
+        })->keys()->all();
     }
 
     public function usesTimestamps() {
@@ -157,6 +239,34 @@ class Object {
     }
 
     public function hasCast($key, $types = null) {
-    	return isset(static::$defs[static::attrName($key)]);
+        if (!$types) {
+        	return isset(static::$defs[static::attrName($key)]);
+        } else if (isset(static::$defs[static::attrName($key)])) {
+        	$type_map = [
+	    		'int' => 'integer',
+	            'integer' => 'integer',
+	            'real' => 'numeric',
+	            'float' => 'numeric',
+	            'double' => 'numeric',
+	            'string' => 'string',
+	            'bool' => 'boolean',
+	            'boolean' => 'boolean',
+	            'object' => null,
+	            'array' => null,
+	            'json' => null,
+	            'collection' => null,
+	            'date' => 'date',
+	            'datetime' => 'date',
+	            'timestamp' => 'date',
+	        ];
+	        foreach ((array) $types as &$type) {
+	        	$type = array_get($type_map, $type);
+	        }
+	        $cast = static::$defs[static::attrName($key)][0];
+	        return in_array($cast, $types);
+        } else {
+        	return parent::hasCast($key, $types);
+        }
+    	
     }
 }
